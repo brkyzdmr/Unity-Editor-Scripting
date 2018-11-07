69947e2ab23e6da66c1e43ef96f1d0886dd92bcf
Berkay Ezdemir <berkayezdemir@hotmail.com>
2018-11-07 23:50:32 +0300

    added separator

---
+++ README.md
@@ -0,0 +1,5 @@
+# Unity-Editor-Scripting
+
+**Separator:**
+
+<img src="Separator/example1.png" width="250px"></img><br>
---
+++ Separator/Scripts/SeparatorExample.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SeparatorExample : MonoBehaviour {
+
+    [Separator("Cover")]
+    public string bookName = "2050";
+    public string authorName = "David Passig";
+    [Separator("Attributes")]
+    public int numOfPages = 387;
+    [Separator("")]
+    [Range(1,5)]
+    public int point = 5;
+
+
+
+
+}
